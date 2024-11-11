42f59f15ff6424493d9c915d2ca7ba09c46f8184
fdricck <s22210431@student.unklab.ac.id>
2024-11-11 09:41:26 +0800

    adding component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Client from "./component/Clients";
+import Features from "./component/Features/index.";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Hero from "./component/Hero";
+import Pricing from "./component/Pricing";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="body-wrap">
+    <Header />
+  <main className="site-content">
+    <Hero />
+    <Client />
+    <Features />
+    <Pricing />
+  </main>
+  <Footer />
+</div>
+
   );
-}
+};
 
 export default App;
